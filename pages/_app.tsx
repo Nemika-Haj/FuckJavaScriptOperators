e5cc580f9457e9f897f46ea6f81fc71202ba1881
Nemika-Haj <nemika@bytestobits.dev>
2025-07-26 18:59:33 +0300

    Force dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import { Provider } from "@/components/ui/provider";
 import type { AppProps } from "next/app";
 
 import "@/styles/globals.css";
+import { DarkMode } from "@/components/ui/color-mode";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider>
+      <DarkMode>
       <Component {...pageProps} />
+      </DarkMode>
     </Provider>
   );
 }
